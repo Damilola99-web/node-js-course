8537d15333cc238b4b7912e618dd496d9f3791f9
Abdul-rasheed <rasheedw622@gmail.com>
2022-08-08 16:07:07 +0100

    learning ejs

--- app.js
+++ app.js
@@ -3,18 +3,25 @@ const express = require('express')
 
 const app = express()
 
+// register app 
+app.set('view engine', 'ejs')
+
 // listen for request 
 
-app.listen(3000);
+app.listen(3005);
 
 app.get('/', (req, res)=> {
-    res.sendFile('./frontEnd/index.html', {root: __dirname})
+    res.render('index')
 })
 app.get('/login', (req, res)=> {
-    res.sendFile('./frontEnd/login.html', {root: __dirname})
+    res.render('login', { test : 'our sign in to our page'})
 })
 
 // redirect 
 app.get('/login-us', (req, res)=>{
     res.redirect('/login')
+})
+
+app.use((req, res)=>{
+    res.status(404).render('404')
 })
